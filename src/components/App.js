d6a085fe19bac0c3ee8b76af1dc1bde63551af9e
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2023-01-08 16:13:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,23 @@ function reducer(state, action) {
         addNum: state.addNum,
         subNum: state.subNum
       };
- 
+ case ACTIONS.DECREMENT:
+      return {
+        count: Number(state.count) - Number(state.addNum),
+        addNum: state.addNum,
+        subNum: state.subNum
+      };
+
+
+case ACTIONS.SET_SUB_NUM:
+      return {
+        count: state.count,
+        addNum: state.addNum,
+        subNum: action.payload
+      };
+
+
+
    
     case ACTIONS.SET_ADD_NUM:
       return {
